e29855f254f26da12be9564ab709051de70ef1fc
Andrey2109 <andrey@podiumcomp.com>
2025-06-11 07:10:10 +0300

    Implement routing and create HomeController with index method

--- public/index.php
+++ public/index.php
@@ -1,6 +1,18 @@
 <?php
 require_once __DIR__ . '/../app/init.php';
+require_once __DIR__ . '/../routes/web.php';
 
-$test = new Test();
 
-var_dump(get_declared_classes());
+
+$request = isset($_GET['url']) ? rtrim($_GET['url'], '/') : '';
+
+
+
+if (array_key_exists($request, $routes)) {
+    $route = explode('@', $routes[$request]);
+    $controllerName =
+        var_dump($route);
+    echo 'It does exists';
+} else {
+    echo "it doesn't exists";
+}
