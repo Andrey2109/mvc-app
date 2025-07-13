d621f70b16c423d52fd03f9899a8f8da7cd166e1
Andrey2109 <python2109@gmail.com>
2025-07-13 06:56:13 +0300

    fix: update links in hero_layout to use base_url for consistent path handling

--- app/views/layouts/hero_layout.php
+++ app/views/layouts/hero_layout.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url('css/style.css') ?>">
 </head>
 <body class="index">
 <nav>
     <ul>
         <li>
-            <a href="index.html">Home</a>
+            <a href="<?php echo base_url('index') ?>">Home</a>
+        </li>
+        <li>
+            <a href="<?php echo base_url('about') ?>">About</a>
         </li>
 
         <!-- When the user is logged in -->
